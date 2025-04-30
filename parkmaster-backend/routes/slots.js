8b1f1b88d0c73043b9b3add308da8b757634bef8
= <=>
2025-04-30 15:49:32 +0530

    deleted parkmaster

--- parkmaster-backend/routes/slots.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const slotController = require('../controllers/slotController');
-
-router.get('/', slotController.getSlots);
-router.post('/', slotController.createSlot);
-router.put('/:id', slotController.updateSlot);
-
-module.exports = router;
