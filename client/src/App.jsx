2eb1e9233a222ecc62383149aaf5de7b93a6f63c
NAVYDON2004 <rithikchandrasekar.22aim@kongu.edu>
2025-05-05 09:52:35 +0530

    client

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ParkingDashboard from './ParkingDashboard'
+import './App.css'
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import 'chart.js/auto'
+
+function App() {
+
+  return (
+    <ParkingDashboard/>
+  )
+}
+
+export default App
